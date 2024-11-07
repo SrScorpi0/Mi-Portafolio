f974cdd2a9de3cb50247caa68cf71077e92f10cd
Axel Rion <axelrion168@gmail.com>
2024-11-07 03:19:53 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("github-button").addEventListener('click', function() {
+    window.location.href = 'https://github.com/SrScorpi0' ;
+});
+
+document.getElementById("linkedin-button").addEventListener('click', function() {
+    window.location.href = 'https://www.linkedin.com/in/axel-rion-914397166/' ;
+});
